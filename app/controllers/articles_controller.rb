482dfd82e2423e17a4a770765f4cbe72e6f02058
Gemille Ford <gemille.ford@gmail.com>
2015-01-27 14:11:35 -0500

    create show article action and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :udpate]
   def index
+  end
 
+  def show
   end
 
   def new
@@ -22,4 +25,8 @@ end
 private
   def article_params
     params.require(:article).permit(:client_name, :vdn, :question, :answer)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
