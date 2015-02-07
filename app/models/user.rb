73a7e71c0852c8e6d7755ca8bc4cc7094311fa7e
rkstarnerd <geille.ford@gmail.com>
2015-02-07 03:21:25 -0500

    add has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
 
+  has_secure_password validations: false
+
   validates :username, presence: true, uniquness: true
+  validates :password, presence: true, on: :create, length: {minimum: 5}
 end
