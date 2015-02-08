7a046ca2886a95fe2a483c5c97b7ed7b4b8c45cc
rkstarnerd <geille.ford@gmail.com>
2015-02-08 07:56:59 -0500

    add create action and set params method for dtcr controller

--- app/controllers/dt_crs_controller.rb
+++ app/controllers/dt_crs_controller.rb
@@ -2,4 +2,19 @@ class DtCrsController < ApplicationController
   def new
     @dt_cr = DtCr.new
   end
+
+  def create
+    @dt_cr = DtCr.new(dt_cr_params)
+
+    if @dt_cr.save
+      flash[:notice] = "Please email the form to dissementations"
+    else
+      render :back
+    end
+  end
+
+  private
+    def dt_cr_params
+      
+    end
 end
