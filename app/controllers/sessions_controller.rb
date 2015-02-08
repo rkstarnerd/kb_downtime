8d5bd8a04537115f1ef63f794d535f039ce093af
rkstarnerd <geille.ford@gmail.com>
2015-02-07 21:14:35 -0500

    correct create action in sessions controller to save session user_id as user.id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
-      session[:user_id]
+      session[:user_id] = user.id
       flash[:notice] = "You've logged in!"
       redirect_to root_path
     else
