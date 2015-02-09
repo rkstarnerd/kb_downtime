06258ffa4dea789c4ee9ce4f49eece44c91c377a
rkstarnerd <geille.ford@gmail.com>
2015-02-09 04:44:10 -0500

    addasset to precompile list

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module KbDowntime
   class Application < Rails::Application
 
-     config.assets.precompile += ['application.css.scss']
+     config.assets.precompile += [ 'application.css.scss' ]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
