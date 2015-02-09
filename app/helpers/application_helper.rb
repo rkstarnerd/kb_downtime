27cd009a3edaa5077a31b44a0194dfa406cfccbb
rkstarnerd <geille.ford@gmail.com>
2015-02-09 02:02:39 -0500

    change z_number so that it has 6 digits instead of 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def z_number(dt_cr_id)
     z_number = @dt_cr.id.to_s
-    if z_number.size < 5
-      z_number.prepend "0" until z_number.size == 5
+    if z_number.size < 6
+      z_number.prepend "0" until z_number.size == 6
     else
       z_number
     end
