0eed3b62a90b7960f5e13b1a7ab0b47cb897de05
rkstarnerd <geille.ford@gmail.com>
2015-02-09 00:41:04 -0500

    define z_number generation method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def z_number(dt_cr_id)
+    z_number = @dt_cr.id.to_s
+    if z_number.size < 5
+      z_number << "0" until z_number.size == 5
+    else
+      z_number
+    end
+  end
 end
