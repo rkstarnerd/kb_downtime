51af0ab0b5fe877207a7dd5fb1ea403d2c2ac0b4
rkstarnerd <geille.ford@gmail.com>
2015-02-09 01:51:13 -0500

    correct z_number method to prepend 0s instead of push

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,10 @@ module ApplicationHelper
   def z_number(dt_cr_id)
     z_number = @dt_cr.id.to_s
     if z_number.size < 5
-      z_number << "0" until z_number.size == 5
+      z_number.prepend "0" until z_number.size == 5
     else
       z_number
     end
+    z_number
   end
 end
