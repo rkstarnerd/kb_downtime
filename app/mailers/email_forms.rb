277049999add594115135e52dec963ae762e185b
rkstarnerd <gemille.ford@gmail.com>
2015-02-24 23:08:05 -0500

    switch email_form arguments

--- app/mailers/email_forms.rb
+++ app/mailers/email_forms.rb
@@ -1,10 +1,10 @@
 class EmailForms < ActionMailer::Base
   default to: "gemille@gmail.com"
 
-  def send_form(user, dt_cr)
+  def email_form(dt_cr, user)
     @user = user
     @dt_cr = dt_cr
-    mail ( :from => @user.username,
-      :subject => 'Downtime Form - VDN: #{@dt_cr.vdn}, #{z_number(@dt_cr.id)}')
+    mail(from:     "#{@user.username}", 
+         subject: 'Downtime Form - VDN: #{@dt_cr.vdn}, #{z_number(@dt_cr.id)}')
   end
 end
