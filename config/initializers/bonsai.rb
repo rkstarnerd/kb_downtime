1d2beed9f22ebff9dd36ce97f36341ab19e5e665
rkstarnerd <gemille.ford@gmail.com>
2015-02-24 21:41:10 -0500

    changed logs to log

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,7 +1,7 @@
 require 'elasticsearch/model'
 
 if ENV['BONSAI_URL']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], log: true})
   BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
 else
   app_name = Rails.application.class.parent_name.underscore.dasherize
