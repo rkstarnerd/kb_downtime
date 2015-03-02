a85ce7af8d16254ecf3e936f83083e05189a805a
rkstarnerd <gemille.ford@gmail.com>
2015-03-01 21:02:06 -0500

    correct syntax error in application.rb and migrate db

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module KbDowntime
         routing_specs: false,
         controller_specs: true,
         request_specs: false
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
