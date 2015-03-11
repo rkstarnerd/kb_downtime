30903fe7747312971ae9b629de86fe68f54ddb4e
Gemille Ford <gemille.ford@gmail.com>
2015-03-11 15:13:10 -0400

    def admin? role for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
+
+  def admin?
+    self.role == 'admin'
+  end
 end
