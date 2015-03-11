9354aa9f49c2e478f41fefa578f7cac4fd34bd68
Gemille Ford <gemille.ford@gmail.com>
2015-03-11 15:19:31 -0400

    require admin for application create, edit, destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   before_action :require_user, except: [:index, :show]
+  before_action :require_admin, only: [:create, :edit, :destroy]
 
   def index
     if params[:q].nil?
