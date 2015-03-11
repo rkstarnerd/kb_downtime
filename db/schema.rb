d571319f40174003f4d8bf67e34aca87e81383a3
Gemille Ford <gemille.ford@gmail.com>
2015-03-11 15:11:49 -0400

    add role column to User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223162905) do
+ActiveRecord::Schema.define(version: 20150311190951) do
 
   create_table "articles", force: true do |t|
     t.string   "client_name"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20150223162905) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "role"
   end
 
 end
