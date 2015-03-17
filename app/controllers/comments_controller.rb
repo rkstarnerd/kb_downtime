808b50bbb6ace1a78920f22d48f9976aff65774d
rkstarnerd <gemille.ford@gmail.com>
2015-03-17 11:37:00 -0400

    create comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class Comments < ApplicationController
+  before_action :require_user
+  before_action :require_admin
+
+  def create
+    @article = Article.find_by(params[:article_id]
+    @comment = @article.comments.build(params.require(:comment).permit(:body)
+    @comment.user_id = current_user
+
+    if @comment.save
+      flash[:notice] = "Your comment was added."
+      redirect_to article_path(@article)
+    else
+      render 'articles/show'
+    end
+  end
+end
