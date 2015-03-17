41ff712053229b96ac4681d3bacfd7c0015d1e59
rkstarnerd <gemille.ford@gmail.com>
2015-03-17 11:24:23 -0400

    create comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311190951) do
+ActiveRecord::Schema.define(version: 20150317152300) do
 
   create_table "articles", force: true do |t|
     t.string   "client_name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150311190951) do
     t.string   "answer"
     t.string   "vdn"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
