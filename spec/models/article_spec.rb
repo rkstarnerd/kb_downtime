0f0ad832faf92030474b4af3749f7def37529197
rkstarnerd <gemille.ford@gmail.com>
2015-04-14 18:13:07 -0400

    test accepts_nested_attributes_for comments

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Article do
   it { should belong_to(:user)}
   it { should have_many(:comments).dependent(:destroy)}
+  it { should accept_nested_attributes_for(:comments) } 
   it { should validate_presence_of(:question)}
   it { should validate_length_of(:question)}
   it { should validate_presence_of(:answer)}
