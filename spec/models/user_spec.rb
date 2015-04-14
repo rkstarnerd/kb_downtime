c60fe1251b488061dcc04f66b0288abec7bb0a7e
rkstarnerd <gemille.ford@gmail.com>
2015-04-14 18:42:35 -0400

    test admin method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,15 @@ describe User do
   it { should validate_presence_of(:password)}
   it { should validate_uniqueness_of(:username)}
   it { should validate_length_of(:password)}
+
+  let(:test_admin) { User.create(username: "admin@test.com", password: "password", role: "admin") }
+  let(:test_user)  { User.create(username: "test@test.com", password: "password", role: "") }
+
+  it "returns false if user is not an admin" do
+    expect(test_user.admin?).to eq(false)
+  end
+  it "returns true if user is an admin" do
+    expect(test_admin.admin?).to eq(true)  
+  end
+
 end
