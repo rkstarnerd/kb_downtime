fbf75841377ea8a2562e164e750838d2d16c3cb8
rkstarnerd <gemille.ford@gmail.com>
2015-04-14 18:32:30 -0400

    test relationship to comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe User do
   it { should have_many(:dt_crs)}
   it { should have_many(:articles)}
+  it { should have_many(:comments)}
   it { should validate_presence_of(:username)}
   it { should validate_presence_of(:password)}
   it { should validate_uniqueness_of(:username)}
