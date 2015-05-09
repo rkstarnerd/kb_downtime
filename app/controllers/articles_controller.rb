64512080bbf9641df3abde812fd46a1b92763399
rkstarnerd <gemille.ford@gmail.com>
2015-05-08 22:02:55 -0400

    set article user_id on create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user_id = current_user.id
     
     if @article.save
       flash[:success] = "Your article was created"
